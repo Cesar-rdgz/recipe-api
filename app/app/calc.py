73e49b8a7a3de6a22a898bf74e34eac929787867
Cesar Rodriguez <cesarrdgz.18.95@gmail.com>
2022-10-02 01:09:14 -0500

    setting up postgres database and installing postgres adaptor

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract y and x and return result."""
+    return y - x
